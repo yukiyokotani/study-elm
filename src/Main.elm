7e8e277376d38018923363c5edd2b499ecb3ea6c
Yuki Yokotani <yokotani.yuki@gmail.com>
2022-07-08 00:53:45 +0900

    linterとformatterの導入

--- src/Main.elm
+++ src/Main.elm
@@ -5,22 +5,35 @@ import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
+
+main : Program () Model Msg
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
 
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Int -> Int
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
 
+
+view : Model -> Html Msg
 view model =
-  div [class "counter-container"]
-    [ button [ onClick Decrement, class "button" ] [ text "-" ] 
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment, class "button" ] [ text "+" ]
-    ]
+    div [ class "counter-container" ]
+        [ button [ onClick Decrement, class "button" ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment, class "button" ] [ text "+" ]
+        ]
